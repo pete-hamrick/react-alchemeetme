9bde3a43ebf5a45d92c3bafca13a7453452665da
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-13 18:25:39 -0800

    took unnecessary imports out of app.js, used context to get the user info in the header and the Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
-import { fetchUser } from './services/user'
 import { useUser } from './context/UserContext'
 
 function App() {
   // inital value should match the data type of end value
   const { user } = useUser()
   return (
-    <Layout user={user}>
-      <Home user={user} />
+    <Layout>
+      <Home />
     </Layout>
   )
 }
