4b2258cf23e6bd698505d7a3a40ee45077ff6082
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-13 18:17:09 -0800

    wrote UserContext file

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,6 @@ import { fetchUser } from './services/user'
 
 function App() {
   // inital value should match the data type of end value
-  const [user, setUser] = useState({})
-
-  useEffect(() => {
-    fetchUser()
-      .then((fetchedUser) => {
-        setUser(fetchedUser)
-      })
-      .catch((error) => {
-        throw new Error(`Error: ${error}`)
-      })
-  }, [])
 
   return (
     <Layout user={user}>
